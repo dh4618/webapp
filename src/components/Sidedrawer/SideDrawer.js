bd8f419f0136d5c39da6331d702ebdfebf4458a2
nym18 <nym18@ic.ac.uk>
2020-06-01 21:14:02 +0800

    added components for sidedrawers

---
+++ src/components/Sidedrawer/SideDrawer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SideDrawer.css'
+
+const SideDrawer = props => (
+    <nav classname="siderDrawer">
+        <ul>
+            <li><a href="/">Log In</a></li>
+            
+        </ul>
+    </nav>
+)
+
+export default SideDrawer;
