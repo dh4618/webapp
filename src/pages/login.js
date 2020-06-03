521839f02daf88e887c893e7dbb6d0d191c60e4d
nym18 <nym18@ic.ac.uk>
2020-06-04 00:31:51 +0800

    added a blank rectangle for login details

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function login() {
     return (
         <div className="login">
+            <div className="loginHero">
             Hello from login page!
+            </div>
+            
         </div>
     )
 }
