10edfa6908de319784be548f4eb22de01e9a0ff8
nym18 <nym18@ic.ac.uk>
2020-06-04 00:38:45 +0800

    added basic button and textfield

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,7 +4,10 @@ export default function login() {
     return (
         <div className="login">
             <div className="loginHero">
-            Hello from login page!
+                Log in
+                <div> Username: <input type="text" name="username"/> </div>
+                <div>Passward:<input type="password" name="password" /></div>
+                <div><input type="submit" name="Log in" /></div>
             </div>
             
         </div>
