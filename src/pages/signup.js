ac531226cbad519eb77d1202a91ed799cf6931f4
Danqing <dh4618@ic.ac.uk>
2020-06-05 03:34:32 +0100

    added radio button for gender selection on sign up page, including 3 options

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function signup() {
     return (
@@ -6,6 +7,20 @@ export default function signup() {
             <div className = "signupHero">
             <br /> <br/>
             <h2>Register</h2>
+            <h3>Your details</h3>
+            <h6>First name *</h6><br />
+            <div className="reg-form-fields"><input type="text" name="firstname"/> </div>
+            Last name *
+            <div className="reg-form-fields"><input type="text" name="lastname"/> </div>
+            Sex *<br />
+            <input type="radio" id="male" name="gender" value="male" />
+            <label for="male">Male</label>
+            <input type="radio" id="female" name="gender" value="female" />
+            <label for="female">Female</label>
+            <input type="radio" id="other" name="gender" value="other" />
+            <label for="other">Other</label>
+
+            <Link to="/explore"><input type="button" value="Create Account" /></Link>
             </div>
         </div>
     )
