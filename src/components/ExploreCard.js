0e2a2df59ea52365d61f024fac7c8990fac258b7
nym18 <nym18@ic.ac.uk>
2020-06-05 22:20:48 +0800

    fixing layout of the explorecard

--- src/components/ExploreCard.js
+++ src/components/ExploreCard.js
@@ -22,23 +22,6 @@ export default function ExploreCard({company}) {
     )
 }
 
-// class ExploreCard extends Component {
-//     render() {
-//         var {image,companyName,sector,subindustry} = this.props;
-//         return (
-//             <div className="company-card">
-//             <div ><img src={require('../assets/images/'+ image)} alt="company logo" className="company-logo"/></div>
-//             <p>{companyName}</p>
-//             <p>#{sector}</p>
-//             <p>#{subindustry}</p>
-//             <div className="addto-wishlist">
-//                 Add to Wish List
-//             </div>
-//             </div>
-//         )
-//     }
-// }
 
-// export default ExploreCard;
 
 
--- src/pages/single-company.js
+++ src/pages/single-company.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import {CompanyContext} from "../context" 
 
 export default class single_company extends Component {
+    constructor(props){
+        super(props)
+
+    }
+    componentDidMount(){
+        
+    }
     render() {
         return (
             <div>
