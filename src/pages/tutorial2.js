2ed41252458d094bfe696c783843852e624602fc
Danqing <dh4618@ic.ac.uk>
2020-06-12 13:28:39 +0100

    arrange tutorial2 page into paragraphs so it is easier for user to read, also make titles bold so it is more clear to the user which section they are reading

--- src/pages/tutorial2.js
+++ src/pages/tutorial2.js
@@ -22,32 +22,32 @@ export default function tutorial2() {
             <img src={img1} style={{maxWidth:800, height:'auto'}}></img>
             <br />
             The series of letters after the name of the company is the ticker symbol, which identifies the company on the stock exchange. 
-            <br / > 
+            <br / > <br />
             How to read a stock chart (in 4 steps)
             <br />
-            Step 1: identify the trend line
+            <bold>Step 1: identify the trend line</bold>
             <br />
             <img src={img2} style={{maxWidth:800, height:'auto'}}></img>
             <br />
             Use your trend line as a first-glance, high-level indicator of something to look into.
-            <br />
-            Step 2: Look for lines of support and resistance
+            <br /><br />
+            <bold>Step 2: Look for lines of support and resistance</bold>
             <br />
             <img src={img3} style={{maxWidth:800, height:'auto'}}></img>
             <br />
             These are levels at which stock stays within over a given period of time. 
             <br />
             Knowing the lines of resistance can help you decide when to buy or sell a stock. However, it is subjective. 
-            <br /> 
-            Step 3: Know when divideends and stock splits occur
+            <br /> <br />
+            <bold>Step 3: Know when divideends and stock splits occur</bold>
             <br />
             <img src={img4} style={{maxWidth:800, height:'auto'}}></img>
             <br />
             A dividend is when the company (the board of directors) decides to give a portion of its earnings back to its shareholders. 
             <br />
             A stock split is a strategic move done by the company’s board of directors to issue more shares of stock to the public.
-            <br />
-            Step 4: Understanding historic trading volumes
+            <br /><br />
+            <bold>Step 4: Understanding historic trading volumes</bold>
             <br />
             <img src={img5} style={{maxWidth:800, height:'auto'}}></img>
             <br />
