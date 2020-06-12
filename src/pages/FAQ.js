e369262e684ab206d5df459aefd6f70ed12d2fc1
Danqing <dh4618@ic.ac.uk>
2020-06-12 02:05:38 +0100

    Changing to tutorial page instead of FAQ, adding simple texts for first tutorial

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -4,7 +4,35 @@ export default function FAQ() {
     return (
         <div className="FAQ">
             <div className="FAQHero">
-            Here we learn basic knowledge 
+            <br /> <br/>
+            <div className = "tab">
+            <br />
+            <h2>Tutorial 1: What is investment? (with definition of basic terms)</h2>
+          
+            <t1>Let's start with what investment actually is!
+            <br />
+            Investment involves steaking money with a company, government etc. in order to earn money on this money.
+            <br />
+            Investment can help with early retirement and quality of life.
+            <br / >
+
+Risk is a valid worry when investing. Some investments are more riskier than others and there is usually a way to estimate the riskiness beforehand.
+ 
+Types of Investments: Stocks, Bonds, Commodities, ETFs, Real Estate, Currency etc.
+ 
+Stock markets are where investments are bought and sold.
+ 
+Compounding causes your investment to grow exponentially over time, thus it is critical to invest even small sums as early as you can in life.
+ 
+Trading is a risky form of short-term investment.
+ 
+Inflation is 2.5% a year, money reduces value on average 2.5% a year and thus if you leave money in an account rather than investing it, it is worth 2.5 less each year.
+ 
+Diversification means investing in different geographies, industries and asset classes.
+
+
+            </t1>
+            </div> 
             </div>
         </div>
     )
