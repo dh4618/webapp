93ad2009293587c4f1f0e309c10da9c18ee23654
Danqing <dh4618@ic.ac.uk>
2020-06-12 13:22:07 +0100

    add images to the text so that users can understand how to read the chart more easily

--- src/pages/tutorial2.js
+++ src/pages/tutorial2.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import img1 from "../assets/images/how-to-read-a-stock-chart.png";
+import img2 from "../assets/images/what-is-a-trend-line.png";
+import img3 from "../assets/images/line-of-support-line-of-resistance.png";
+import img4 from "../assets/images/what-is-a-stock-split.png";
+import img5 from "../assets/images/stock-trading-volumes.png";
 
 export default function tutorial2() {
     return (
@@ -14,15 +19,21 @@ export default function tutorial2() {
             <br />
             Let's take a look at a typical stock chart below:
             <br />
+            <img src={img1} style={{maxWidth:800, height:'auto'}}></img>
+            <br />
             The series of letters after the name of the company is the ticker symbol, which identifies the company on the stock exchange. 
             <br / > 
             How to read a stock chart (in 4 steps)
             <br />
             Step 1: identify the trend line
             <br />
+            <img src={img2} style={{maxWidth:800, height:'auto'}}></img>
+            <br />
             Use your trend line as a first-glance, high-level indicator of something to look into.
             <br />
             Step 2: Look for lines of support and resistance
+            <br />
+            <img src={img3} style={{maxWidth:800, height:'auto'}}></img>
             <br />
             These are levels at which stock stays within over a given period of time. 
             <br />
@@ -30,11 +41,15 @@ export default function tutorial2() {
             <br /> 
             Step 3: Know when divideends and stock splits occur
             <br />
+            <img src={img4} style={{maxWidth:800, height:'auto'}}></img>
+            <br />
             A dividend is when the company (the board of directors) decides to give a portion of its earnings back to its shareholders. 
             <br />
             A stock split is a strategic move done by the company’s board of directors to issue more shares of stock to the public.
             <br />
             Step 4: Understanding historic trading volumes
+            <br />
+            <img src={img5} style={{maxWidth:800, height:'auto'}}></img>
             <br />
             With high volumes comes greater ease when buying or selling. If a lot of people are trading the stock that day, you should be able to buy or sell it quickly.
             <br /><br />
