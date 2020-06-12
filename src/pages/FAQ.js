fb942f199671790181601f98c1d3ebe9bda9ab28
Danqing <dh4618@ic.ac.uk>
2020-06-12 02:37:43 +0100

    change DOB field to type date on signup page, adding next page button on tutorial 1 page

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -9,28 +9,27 @@ export default function FAQ() {
             <br />
             <h2>Tutorial 1: What is investment? (with definition of basic terms)</h2>
           
-            <t1>Let's start with what investment actually is!
+            <t1>Let's start with what investment actually is and why you should start investing NOW!
             <br />
             Investment involves steaking money with a company, government etc. in order to earn money on this money.
             <br />
             Investment can help with early retirement and quality of life.
             <br / >
-
-Risk is a valid worry when investing. Some investments are more riskier than others and there is usually a way to estimate the riskiness beforehand.
- 
-Types of Investments: Stocks, Bonds, Commodities, ETFs, Real Estate, Currency etc.
- 
-Stock markets are where investments are bought and sold.
- 
-Compounding causes your investment to grow exponentially over time, thus it is critical to invest even small sums as early as you can in life.
- 
-Trading is a risky form of short-term investment.
- 
-Inflation is 2.5% a year, money reduces value on average 2.5% a year and thus if you leave money in an account rather than investing it, it is worth 2.5 less each year.
- 
-Diversification means investing in different geographies, industries and asset classes.
-
-
+            Risk is a valid worry when investing. Some investments are more riskier than others and there is usually a way to estimate the riskiness beforehand.
+            <br />
+            Types of Investments: Stocks, Bonds, Commodities, ETFs, Real Estate, Currency etc.
+            <br />
+            Stock markets are where investments are bought and sold.
+            <br />
+            Compounding causes your investment to grow exponentially over time, thus it is critical to invest even small sums as early as you can in life.
+            <br />
+            Trading is a risky form of short-term investment.
+            <br />
+            Inflation is 2.5% a year, money reduces value on average 2.5% a year and thus if you leave money in an account rather than investing it, it is worth 2.5 less each year.
+            <br /> 
+            Diversification means investing in different geographies, industries and asset classes.
+            <br /><br />
+            <input type="button" value="next page" />
             </t1>
             </div> 
             </div>
