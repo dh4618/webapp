e4aeb5e98255b4d9cb8fc961c7a449118c2f454f
Danqing <dh4618@ic.ac.uk>
2020-06-17 06:57:53 +0100

    add tips for GICS sectors, subindustry and stock symbol

--- frontend/src/components/CompaniesFilter.js
+++ frontend/src/components/CompaniesFilter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useContext} from 'react'
 import {CompanyContext} from  '../context'
+import Tips from "./Tips"
 
 //get all unique values
 const getUnique = (items, value) => {
@@ -37,9 +38,12 @@ export default function CompaniesFilter({companies}) {
     return (
         <section className="filter-container">
             <form className="filter-form">
-                {/*select sector*/}
+                {/select sector/}
                 <div className = "form-group">
-                    <label htmlFor="sector"> Economic Sector</label>
+                    <label htmlFor="sector"> 
+                    <Tips explanation="A sector is an area of the 
+                economy in which businesses share the same or 
+                a related product or service."/>GICS Sector</label>
                     <select 
                         name="sector" 
                         id="sector" 
@@ -51,11 +55,13 @@ export default function CompaniesFilter({companies}) {
                     </select>
                        
                 </div>
-                {/*end select sector*/}
+                {/end select sector/}
 
-                {/*select subindustry*/}
+                {/select subindustry/}
                 <div className = "form-group">
-                    <label htmlFor="subindustry"> Economic Subindustries</label>
+                
+                <label htmlFor="subindustry"> 
+                <Tips explanation="An industry group is a classification method for individual companies or stocks that creates groupings based on common lines of business."/> GICS Subindustries</label>
                     <select 
                         name="subindustry" 
                         id="subindustry" 
@@ -66,16 +72,16 @@ export default function CompaniesFilter({companies}) {
                     {subindustries}
                     </select>
                 </div>
-                {/*end select subindustry*/}
+                {/end select subindustry/}
 
-                {/*select price*/}
+                {/select price/}
                 <div className = "form-group">
                     <label htmlFor="price"> Price ${price}</label>
                     <input type="range" name="price" 
                     min={minPrice} max={maxPrice} id="price" value={price}
                     onChange={handleChange} className="form-control"/>
                 </div>
-                {/*end select price*/}
+                {/end select price/}
             </form>
         </section>
     )
